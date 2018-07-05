15df51172d57fe7b9fa044ee4d3e60d577f84aa9
Tanmay <tanmay.kadam@qed42.com>
2018-07-06 01:53:03 +0530

    Routing demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DatabasesComponent } from './databases/databases.component';
+import { OperatingsystemsComponent } from './operatingsystems/operatingsystems.component';
+import { ProgramminglanguagesComponent } from './programminglanguages/programminglanguages.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path : 'operatingsystems', component:OperatingsystemsComponent},
+  {path : 'programming' ,component:ProgramminglanguagesComponent},
+  {path : 'databases' ,component:DatabasesComponent},
+  { path: '', component:OperatingsystemsComponent},
+  {path : '**', component:PagenotfoundComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OperatingsystemsComponent } from './operatingsystems/operatingsystems.component';
+import { ProgramminglanguagesComponent } from './programminglanguages/programminglanguages.component';
+import { DatabasesComponent } from './databases/databases.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OperatingsystemsComponent,
+    ProgramminglanguagesComponent,
+    DatabasesComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
